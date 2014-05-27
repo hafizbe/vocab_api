0603ecd9c90bdd30a1dc4b63ee2ea5a34d12eaca
Be Hafiz <hafizbe@gmail.com>
2014-05-27 20:59:14 +0200

    Fix date format

--- lib/service_convertor.rb
+++ lib/service_convertor.rb
@@ -1,5 +1,5 @@
 class ServiceConvertor
   def self.date_to_fr(date)
-    Date.strptime(date.to_s, "%Y-%m-%d").strftime("%m/%d/%Y")
+    Date.strptime(date.to_s, "%Y-%m-%d").strftime("%d/%m/%Y")
   end
 end
