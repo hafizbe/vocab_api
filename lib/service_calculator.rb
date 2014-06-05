b6f706321e3ac3e3c6e6c46e62e71b85e5c18ed6
Be Hafiz <hafizbe@gmail.com>
2014-06-05 18:43:44 +0200

    Modification formule pourcentage

--- lib/service_calculator.rb
+++ lib/service_calculator.rb
@@ -2,7 +2,7 @@ class ServiceCalculator
   def self.percentage_sura(nb_cards, points)
     total = 0
     unless points == 0
-      total = (points.to_f / (nb_cards * 3).to_f * 100).round 2
+      total = (points.to_f / (nb_cards * 5).to_f * 100).round 2
     end
 
     total
